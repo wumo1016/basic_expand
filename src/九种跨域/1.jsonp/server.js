29cc91a9e1aa6a10a160f5682c8f58635e107c2a
wumo1016 <760478279@qq.com>
2020-12-25 14:13:04 +0800

    update

---
+++ src/九种跨域/1.jsonp/server.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const app = express()
+
+app.get('/test', function(req, res) {
+  const { wd, cb } = req.query
+  res.end(`${cb}('me too')`)
+})
+
+app.listen(3000)
