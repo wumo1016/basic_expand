4bb39dc512fa3006e7a24007e97a5de4d2b820f3
wumo1016 <760478279@qq.com>
2021-02-24 13:52:40 +0800

    update

--- src/模块化/commonjs/a.js
+++ src/模块化/commonjs/a.js
@@ -1,6 +1,12 @@
 
-const b = require('./b')
-console.log(b, 'b');
-module.exports = {
-  name: 'a'
-}
+// const b = require('./b')
+// console.log(b, 'b');
+// module.exports = {
+//   name: 'a'
+// }
+
+let a = 1
+setTimeout(() => {
+  a = 2
+}, 1000)
+module.exports = a
--- src/模块化/commonjs/b.js
+++ src/模块化/commonjs/b.js
@@ -1,6 +1,11 @@
 
+// const a = require('./a')
+// console.log(a, 'a');
+// module.exports = {
+//   name: 'b'
+// }
+
 const a = require('./a')
-console.log(a, 'a');
-module.exports = {
-  name: 'b'
-}
+setTimeout(() => {
+  console.log(a);
+}, 2000)
