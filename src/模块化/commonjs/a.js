759e538c7c12ccd5549648439514460632dc3223
wumo1016 <760478279@qq.com>
2021-02-24 10:10:58 +0800

    done(commonjs)

---
+++ src/模块化/commonjs/a.js
@@ -0,0 +1,6 @@
+
+const b = require('./b')
+console.log(b, 'b');
+module.exports = {
+  name: 'a'
+}
---
+++ src/模块化/commonjs/b.js
@@ -0,0 +1,6 @@
+
+const a = require('./a')
+console.log(a, 'a');
+module.exports = {
+  name: 'b'
+}
