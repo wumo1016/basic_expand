4f0f31210d918c0ff30ad0719f2cbd5fd9ac4be7
wumo1016 <760478279@qq.com>
2021-09-26 17:55:38 +0800

    chore(继承)

--- src/前端基础/8.继承.js
+++ src/前端基础/8.继承.js
@@ -1,62 +1,112 @@
+/* --------------- 1.原型链继承 -------------------- */
+// 缺点：父类上的所有引用属性将会被所有子类共享
+;(function () {
+  function Parent() {
+    this.names = []
+  }
+  function Child() {}
+  Child.prototype = new Parent()
 
-function _extends(Child, Father) {
-  Child.__proto__ = Father;//继承静态属性的方法
-  function Temp() {
-      //constructor指向子类的构造函数
-      //this.constructor = Child;
-  }
-  //Temp.constructor = Child;
-  //(__.prototype = b.prototype, new __());
-  Temp.prototype = Father.prototype;//Father
-  Temp.prototype.constructor = Child;
-  Child.prototype = new Temp();
+  const child1 = new Child()
+  child1.names.push(123)
+  const child2 = new Child()
 
-  /**
-   * 1.会有两个私有属性
-   * 2.为了避免在此继承私有属性
-   * 3.会把Father 上的私有属性都变成Child实例 的公有属性，而且属性都是错的
-   */
-  /*   Child.prototype = new Father();
-    Child.prototype.constructor = Child; */
-}
+  console.log(child1.names) // [ 123 ]
+  console.log(child2.names) // [ 123 ]
+})
 
-var Father = (function () {
-  function Father(name) {
-      this.name = name;
+/* --------------- 2.冒充继承(借用构造函数继承) -------------------- */
+// 缺点: 无法继承原型上的属性
+;(function () {
+  function Parent(name) {
+    this.names = [name]
   }
-  Father.prototype.getName = function () {
-      console.log(this.name);
+  Parent.prototype.age = 18
+  function Child(name) {
+    Parent.call(this, name)
   }
-  Father.staticFatherName = 'FatherName';
-  Father.staticGetFatherName = function () {
-      console.log(Father.staticFatherName);
-  }
-  return Father;
-})();
-var Child = (function (_super) {//_super=Father
-  _extends(Child, _super);
-  function Child(name, age) {
-      //this其实是指向子类的实例 new Object {}子类的实例
-      //是在调用父类的构造函数，初始化父类的私有属性
-      _super.call(this, name);
-  }
-  Child.prototype.getAge = function () {
-      console.log(this.name);
-  }
-  Child.staticChildName = 'ChildName';
-  Child.staticGetChildName = function () {
-      console.log(Child.staticChildName);
-  }
-  return Child;
-})(Father);
 
-let child = new Child('zhufeng', 10);
-console.log(child.constructor);
-console.log(child.constructor === Father);
-console.log(child.constructor === Child);
-child.getName();
-child.getAge();
-Child.staticGetChildName();
-Child.staticGetFatherName();
+  const child1 = new Child('111')
+  child1.names.push(222)
+  const child2 = new Child('222')
+  console.log(child1.names) // [ 111, 222 ]
+  console.log(child2.names) // [ 222 ]
+  console.log(child1.age) // undefined
+})
 
-//export { };
+/* --------------- 3.组合继承 -------------------- */
+;(function () {
+  function Parent() {
+    this.names = []
+  }
+  Parent.prototype.age = 18
+  Parent.prototype.obj = {}
+  function Child() {
+    Parent.call(this)
+  }
+  Child.prototype = new Parent()
+  Child.prototype.constructor = Child
+
+  const child1 = new Child()
+  const child2 = new Child()
+  child1.obj.name = 'wyb'
+
+  console.log(child1.names) // [ 123 ]
+  console.log(child2.names) // []
+  console.log(child1.age) // 18
+  console.log(child1.obj) // { name: 'wyb' }
+  console.log(child2.obj) // { name: 'wyb' }
+})
+
+/* --------------- 4.原型式继承 -------------------- */
+;(function () {
+  function createObj(o) {
+    function F() {}
+    F.prototype = o
+    return new F()
+  }
+
+  const obj = {
+    names: [],
+    age: 0
+  }
+  const obj1 = createObj(obj)
+  const obj2 = createObj(obj)
+  obj1.names.push('wyb')
+  obj1.age = 18
+  console.log(obj1.names) // [ 'wyb' ]
+  console.log(obj2.names) // [ 'wyb' ]
+  console.log(obj1.age) // 18
+  console.log(obj2.age) // 0
+})
+
+/* --------------- 4.寄生式继承 -------------------- */
+;(function () {
+  function createObj(o) {
+    var clone = Object.create(o)
+    clone.sayName = function () {
+      console.log('hi')
+    }
+    return clone
+  }
+  const parent = {}
+  const child = createObj(parent)
+})
+
+/* --------------- 5.寄生组合式继承 -------------------- */
+// 组合继承调用了两次父类构造函数
+;(function () {
+  function createObj(o) {
+    function F() {}
+    F.prototype = o
+    return new F()
+  }
+  function Parent() {
+    console.log(123)
+  }
+  function Child() {
+    Parent.call(this)
+  }
+  Child.prototype = createObj(Parent.prototype) // 等同于使用 Object.create
+  Child.prototype.constructor = Child
+})()
