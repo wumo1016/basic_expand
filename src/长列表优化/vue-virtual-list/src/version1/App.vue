8a81cff192f02f2c5519ac030a93e0865ed473aa
wumo1016 <760478279@qq.com>
2022-03-08 19:25:24 +0800

    chore

---
+++ src/长列表优化/vue-virtual-list/src/version1/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <VirtualList :size="40" :remain="20" :items="items" />
+</template>
+
+<script setup>
+import VirtualList from './virtual-list.vue'
+
+let items = []
+for (let i = 0; i < 1000; i++) {
+  items.push({ id: i, value: i })
+}
+</script>
+
+<style>
+#app {
+  border: 1px solid blue;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
