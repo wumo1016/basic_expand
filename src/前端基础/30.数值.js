9be0555ddc6a37664e653c262a9f2475189f0a22
wumo1016 <760478279@qq.com>
2022-03-26 17:37:17 +0800

    chore

---
+++ src/前端基础/30.数值.js
@@ -0,0 +1,57 @@
+/* ----------------------------------------- 数值分隔符  ----------------------------------------- */
+/* 注意事项
+- 1.不能放在数值的最前面或最后面
+- 2.不能两个以上的分隔符在一起
+- 3.小数点前后不能有分隔符
+- 4.科学计数法中的eE前后不能有分隔符
+*/
+;(function () {
+  console.log(1_1 === 11)
+  console.log(11_11_11 === 111111)
+  console.log(111_111_111 === 111111111)
+})
+
+/* ----------------------------------------- Number静态属性  ----------------------------------------- */
+/* 
+- MAX_SAFE_INTEGER: 最大安全整数(Math.pow(2, 53) - 1)(9007199254740991)
+- MIN_SAFE_INTEGER: 最小安全整数(-9007199254740991)
+*/
+;(function () {
+  console.log(Number.MAX_SAFE_INTEGER === 9007199254740991)
+  console.log(Number.MIN_SAFE_INTEGER === -9007199254740991)
+})
+
+/* ----------------------------------------- Number静态方法  ----------------------------------------- */
+/* 
+- parseInt: 与全局方法一致(将指定基数的字符串转换为十进制数) => parseInt(string, radix)
+- parseFloat: 与全局方法一致(将字符串转换为浮点数)
+- isFinite: 判断是是否不是 Infinity
+- isInteger: 判断是否是整数
+- isSafeInteger: 判断是否是安全整数
+*/
+;(function () {
+  console.log(Number.parseInt === parseInt)
+  console.log(Number.parseFloat === parseFloat)
+
+  console.log(isFinite(123)) // true
+  console.log(isFinite(Infinity)) // false
+
+  console.log(Number.isInteger(123)) // true
+  console.log(Number.isInteger(123.1)) // false
+
+  console.log(Number.isSafeInteger(123)) // true
+  console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1)) // false
+})
+
+/* ----------------------------------------- Math静态方法  ----------------------------------------- */
+/* 
+- trunc: 去除小数部分
+- sign: 用于判断一个数是否大于0(大于0返回1 小于0返回-1 等于0返回0)
+*/
+;(function () {
+  console.log(Math.trunc(1.23)) // 1
+
+  console.log(Math.trunc(100)) // 1
+  console.log(Math.trunc(-100)) // -1
+  console.log(Math.trunc(0)) // 0
+})
