0b863cfa320eaa70c051fc9ae7f4027920348c8e
wumo1016 <760478279@qq.com>
2022-06-16 14:37:16 +0800

    chore(commonjs)

--- 模块化/commonjs/a.js
+++ 模块化/commonjs/a.js
@@ -1,12 +1,18 @@
-
 // const b = require('./b')
 // console.log(b, 'b');
 // module.exports = {
 //   name: 'a'
 // }
 
-let a = 1
+let a = 1,
+  obj = {
+    name: 'wyb'
+  }
 setTimeout(() => {
   a = 2
+  obj.name = 'love'
 }, 1000)
-module.exports = a
+module.exports = {
+  a,
+  obj
+}
--- 模块化/commonjs/b.js
+++ 模块化/commonjs/b.js
@@ -1,11 +1,40 @@
-
 // const a = require('./a')
 // console.log(a, 'a');
 // module.exports = {
 //   name: 'b'
 // }
 
-const a = require('./a')
+const { a, obj } = require('./a')
+console.log(a)
+console.log(obj.name)
 setTimeout(() => {
-  console.log(a);
+  console.log(a)
+  console.log(obj.name)
 }, 2000)
+
+/* 
+// a.js
+exports.done = false;
+var b = require('./b.js');
+console.log('在 a.js 之中，b.done = ', b.done);
+exports.done = true;
+console.log('a.js 执行完毕');
+
+// b.js
+exports.done = false;
+var a = require('./a.js');
+console.log('在 b.js 之中，a.done = ', a.done);
+exports.done = true;
+console.log('b.js 执行完毕');
+
+// main.js
+var a = require('./a.js');
+var b = require('./b.js');
+console.log('在 main.js 之中, a.done=, b.done=', a.done, b.done);
+
+在 b.js 之中，a.done = false
+b.js 执行完毕
+在 a.js 之中，b.done = true
+a.js 执行完毕
+在 main.js 之中, a.done=true, b.done=true
+*/
