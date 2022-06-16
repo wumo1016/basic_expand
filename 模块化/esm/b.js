2db15c303653295ac63985e3bce771f7f1cce6a2
wumo1016 <760478279@qq.com>
2022-06-16 14:37:33 +0800

    chore(esm)

--- 模块化/esm/b.js
+++
@@ -1,7 +0,0 @@
-
-import d, { aa, b } from './a.js'
-
-setTimeout(() => {
-  console.log(b);
-  console.log(d);
-}, 2000)
---
+++ 模块化/esm/b.mjs
@@ -0,0 +1,6 @@
+import d, { aa, b } from './a.mjs'
+
+setTimeout(() => {
+  console.log(b)
+  console.log(d)
+}, 2000)
