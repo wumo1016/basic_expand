8085362be10bb6bacfa47c0af1f5a1603aba53e2
wumo1016 <760478279@qq.com>
2023-01-06 17:14:07 +0800

    chore

--- test/index.js
+++ test/index.js
@@ -1,1 +1,14 @@
 /* bug修复之undefined*/
+
+/* 
+const iframe = document.getElementById('tgb').contentWindow
+iframe.document.querySelector('.other .mod_pagenav_turn input').value = 110
+iframe.document.querySelector('.other .mod_pagenav_turn button').click()
+
+iframe.document.querySelector('#btnBatchBottom').click()
+setTimeout(() => {
+  iframe.document.querySelector('#chkSelectAllBottom').click()
+  iframe.document.querySelector('#btnDeleteBatchBottom').click()
+  setTimeout(() => document.querySelector('.qz_dialog_layer_sub').click(), 200)
+}, 500)
+ */
