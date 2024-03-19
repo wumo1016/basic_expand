6965475e68c2ff2461fa90e8e9e720b1a184e4e4
wumo1016 <760478279@qq.com>
2024-03-19 10:33:55 +0800

    refactor: Web Component

--- 前端基础/html/api/Web Component/collapse/js/index.js
+++ 前端基础/html/api/Web Component/collapse/js/index.js
@@ -7,15 +7,18 @@ window.customElements.define('wm-collapse-item', CollapseItem)
 
 // 设置组件的默认显示状态
 let activeList = ['1']
-function setActive(){
-  document.querySelector('wm-collapse').setAttribute('active', JSON.stringify(activeList))
+
+function setActive() {
+  document
+    .querySelector('wm-collapse')
+    .setAttribute('active', JSON.stringify(activeList))
 }
 setActive()
 
 document.querySelector('wm-collapse').addEventListener('activeChange', e => {
   const { name } = e.detail
   const index = activeList.findIndex(v => v === name)
-  if(index > -1){
+  if (index > -1) {
     activeList.splice(index, 1)
   } else {
     activeList.push(name)
