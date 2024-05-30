b6cbe5a05cc3e61031530faf41c48e04fef34991
wumo1016 <760478279@qq.com>
2024-05-30 14:06:48 +0800

    chore: test

---
+++ test/index.js
@@ -0,0 +1,22 @@
+const list = Array.from(document.querySelectorAll('.r-q4m81j')).slice(14, 16)
+const submit = document.querySelector('#prd-botnav-rightbtn-txt div')
+function run() {
+  list.forEach((dom, i) => {
+    if (i === 0) {
+      dom.click()
+    } else {
+      dom.click()
+      const t = setInterval(() => {
+        const text = submit.innerText
+        if (text === '暂时缺货') {
+          clearInterval(t)
+          run()
+        } else if (text === '立即购买') {
+          clearInterval(t)
+          submit.click()
+        }
+      }, 500)
+    }
+  })
+}
+run()
