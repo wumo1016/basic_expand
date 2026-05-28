d6e53db19b4efe7fe161049c74ed36328f53ead1
wumo1016 <760478279@qq.com>
2026-05-28 10:35:25 +0800

    chore: 测试

--- test/index.js
+++ test/index.js
@@ -1,24 +1,3 @@
-const list = Array.from(document.querySelectorAll('.r-q4m81j')).slice(14, 16)
-const submit = document.querySelector('#prd-botnav-rightbtn-txt div')
-function run() {
-  list.forEach((dom, i) => {
-    if (i === 0) {
-      dom.click()
-    } else {
-      dom.click()
-      setTimeout(() => {
-        const t = setInterval(() => {
-          const text = submit.innerText
-          if (text === '暂时缺货') {
-            clearInterval(t)
-            run()
-          } else if (text === '立即购买') {
-            clearInterval(t)
-            submit.click()
-          }
-        }, 500)
-      }, 500)
-    }
-  })
-}
-run()
+import Decimal from 'decimal.js'
+
+console.log(new Decimal(2).div(0))
